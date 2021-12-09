032f60afd449b9aa36275a2e2cd15bafa97f14fa
ibasaw54 <awwsb41@gmail.com>
2021-12-09 17:17:57 +0900

    amplify2

--- nuru-front/src/OAuth.js
+++ nuru-front/src/OAuth.js
@@ -1,5 +1,5 @@
 const rest_api_key = "c38ee04e16631dabbb8e43a1ed540d05"
-const redirect_URI = "https://localhost:3000/oauth/callback/kakao-login"
+const redirect_URI = "/oauth/callback/kakao-login"
 
 
 export const kakao_login_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${rest_api_key}&redirect_uri=${redirect_URI}&response_type=code`
